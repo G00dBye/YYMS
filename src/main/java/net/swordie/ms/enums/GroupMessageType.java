1de816fc842b6598d5b4b7896b6ab0ee8f7cdcfb
M <M>
2020-07-28 09:01:01 +0800

    no message

---
+++ src/main/java/net/swordie/ms/enums/GroupMessageType.java
@@ -0,0 +1,14 @@
+package net.swordie.ms.enums;
+
+/**
+ * @author Sjonnie Created on 7/21/2018.
+ */
+public enum GroupMessageType {
+    Buddy,
+    Party,
+    Guild,
+    Alliance,
+    None4,
+    None5,
+    Expedition
+}
---
+++ src/main/java/net/swordie/ms/enums/SpellTraceScrollType.java
@@ -0,0 +1,10 @@
+package net.swordie.ms.enums;
+
+/**
+ * @author Sjonnie Created on 8/18/2018.
+ */
+public enum SpellTraceScrollType {
+    Normal,
+    Innocence,
+    CleanSlate
+}
